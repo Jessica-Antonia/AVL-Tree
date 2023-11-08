01dc83bd475c585c74c7144597dea8bb094c70d7
Jessica-Antonia <jessica.antonia@gmail.com>
2023-11-07 20:31:57 -0500

    stuff

--- Node.cpp
+++ Node.cpp
@@ -1,3 +1,5 @@
 //CS3100 Project 4
 //Jessica Venema
 
+#include "Node.h"
+#include<string>
--- Node.h
+++ Node.h
@@ -1,3 +1,28 @@
 //CS3100 Project 4
 //Jessica Venema
 
+#include<string>
+
+using namespace std;
+
+class Node {
+
+    int key = 0;
+    string thing = "";
+
+    Node();
+
+    Node(int key, string thing);
+
+    Node(const Node& n);
+
+    ~Node();
+
+    
+
+
+
+
+
+
+};
